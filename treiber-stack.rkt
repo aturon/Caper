7068ed1a70aca414b3b2578e9fe950755db7b1af
Aaron Turon <turon@ccs.neu.edu>
2012-05-01 15:20:06 -0400

    treiber stack - more idomatic

--- treiber-stack.rkt
+++ treiber-stack.rkt
@@ -1,35 +1,33 @@
-(module treiber-stack racket
+#lang scheme
 
-  (require scheme/future)
-  (provide make-treiber-stack push pop)
+(require scheme/future)
+(provide make-treiber-stack push pop)
 
-  (struct tstack (head))
+(struct tstack (head))
 
-  (define (make-treiber-stack)
-    (tstack (box '())))
+(define (make-treiber-stack)
+  (tstack (box '())))
 
-  (define (push s x)
-    (define head (tstack-head s))
-    (define node (mcons x '()))
-    (let retry ()
-      (define snapshot (unbox head))
-      (set-mcdr! node snapshot)
-      (unless (box-cas! head snapshot node)
-	(retry))))
+(define (push s x)
+  (define head (tstack-head s))
+  (define node (mcons x '()))
+  (let retry ()
+    (define snapshot (unbox head))
+    (set-mcdr! node snapshot)
+    (unless (box-cas! head snapshot node)
+      (retry))))
 
-  (define (pop s 
-	       [failure-result 
-		(lambda () 
-		  (error (string-append "pop: stack is empty")))])
-    (define head (tstack-head s))
-    (let retry ()
-      (define snapshot (unbox head))
-      (cond
-        [(eq? snapshot '()) 
-	 (if (proc? failure-result) (failure-result) failure-result)]
-	[(box-cas! head snapshot (mcdr snapshot))
-	 (mcar snapshot)]
-	[else 
-	 (retry)])))
-
-)
+(define (pop s 
+	     [failure-result 
+	      (lambda () 
+		(error (string-append "pop: stack is empty")))])
+  (define head (tstack-head s))
+  (let retry ()
+    (define snapshot (unbox head))
+    (cond
+      [(eq? snapshot '()) 
+       (if (proc? failure-result) (failure-result) failure-result)]
+      [(box-cas! head snapshot (mcdr snapshot))
+       (mcar snapshot)]
+      [else 
+       (retry)])))
