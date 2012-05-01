12b371655f21fcb277a9f2e6a23d2a027bb18d0b
Sam Tobin-Hochstadt <samth@racket-lang.org>
2012-05-01 18:52:26 -0400

    Provide option to switch to immutable cons, also inlined push/pop
    Immutable, inlined version is abotu 40% faster for sequential
    performance, no noticeable change in any parallel version.
    
    Parallel timings still very noisy.

--- treiber-stack.rkt
+++ treiber-stack.rkt
@@ -2,35 +2,79 @@
 
 ; a by-hand implementation of Treiber's stack, using mutable cons cells
 
-(require racket/future)
-(provide make-treiber-stack push pop)
+(require racket/future racket/performance-hint racket/unsafe/ops)
+(provide make-treiber-stack push push* pop pop*)
 
 (struct tstack (head))
 
 (define (make-treiber-stack)
   (tstack (box '())))
 
+#;
+(begin
+  (define-syntax-rule (alloc-node x) (mcons x '()))
+  (define-syntax-rule (update-node node x ss)
+    (unsafe-set-mcdr! node ss))
+  (define-syntax-rule (hd x) (unsafe-mcar x))
+  (define-syntax-rule (tl x) (unsafe-mcdr x)))
+
+
+(begin
+  (define-syntax-rule (alloc-node x) (cons x '()))
+  (define-syntax-rule (update-node node x ss) 
+    (define node (cons x ss)))
+  (define-syntax-rule (hd x) (unsafe-car x))
+  (define-syntax-rule (tl x) (unsafe-cdr x)))
+
+
+(define-syntax-rule (push* s x)
+  (let ()
+    (define _x x)
+    (define head (tstack-head s))
+    (define node (alloc-node _x))
+    (let retry ()
+      (define snapshot (unsafe-unbox* head))
+      (update-node node _x snapshot)
+      (unless (box-cas! head snapshot node)
+        (retry)))))
+
 (define (push s x)
   (define head (tstack-head s))
-  (define node (mcons x '()))
+  (define node (alloc-node x))
   (let retry ()
-    (define snapshot (unbox head))
-    (set-mcdr! node snapshot)
+    (define snapshot (unsafe-unbox* head))
+    (update-node node x snapshot)
     (unless (box-cas! head snapshot node)
       (retry))))
 
 (define (pop s 
-	     [failure-result 
-	      (λ () (raise-type-error 'pop "non-empty stack" s))])
+             [failure-result 
+              (λ () (raise-type-error 'pop "non-empty stack" s))])
   (define head (tstack-head s))
   (let retry ()
-    (define snapshot (unbox head))
+    (define snapshot (unsafe-unbox* head))
     (cond
       [(eq? snapshot '()) 
        (if (procedure? failure-result) (failure-result) failure-result)]
-      [(box-cas! head snapshot (mcdr snapshot))
-       (mcar snapshot)]
+      [(box-cas! head snapshot (tl snapshot))
+       (hd snapshot)]
       [else 
        (retry)])))
+
+
+(define-syntax-rule (pop* s)
+  (let ()
+    [define failure-result 
+      (λ () (raise-type-error 'pop "non-empty stack" s))]
+    (define head (tstack-head s))
+    (let retry ()
+      (define snapshot (unsafe-unbox* head))
+      (cond
+        [(eq? snapshot '()) 
+         (if (procedure? failure-result) (failure-result) failure-result)]
+        [(box-cas! head snapshot (tl snapshot))
+         (hd snapshot)]
+        [else 
+         (retry)]))))
 
 
