08d8b6539619f45dcebb4345117008e54201b883
Sam Tobin-Hochstadt <samth@racket-lang.org>
2012-05-01 16:23:18 -0400

    better generated code, inexect-ms

--- bench.rkt
+++ bench.rkt
@@ -4,24 +4,23 @@
 
 (provide measure-throughput)
 
-;; measure-throughput: syntax -> syntax
+; total iterations is currently a magic constant, but should
+; eventually be dynamically determined to be large enough to
+; get an accurate measurement.
+(define iters 100000000)
 
 ;; (measure-throughput code threads) measures how many times on average `code'
 ;; executes, per microsecond, while run in `threads' parallel threads.
 (define-syntax (measure-throughput stx)
   (syntax-parse stx
     [(_ code threads:integer)
-     (with-syntax ([(tid ...) (generate-temporaries (build-list (syntax-e #'threads) (lambda (x) x)))]
-                   ; total iterations is currently a magic constant, but should
-                   ; eventually be dynamically determined to be large enough to
-                   ; get an accurate measurement.
-                   [iters #'100000000])
+     (with-syntax ([(tid ...) (generate-temporaries (build-list (syntax-e #'threads) (λ _ 'tid)))])
        #'(let ()
            (define iters/thread (floor (/ iters threads)))
            (define (thread-body)
              (for ([i (in-range iters/thread)]) code))
-           (define start-time (current-milliseconds))
+           (define start-time (current-inexact-milliseconds))
            (let ([tid (future thread-body)] ...)
              (touch tid) ...)
-           (define end-time (current-milliseconds))
+           (define end-time (current-inexact-milliseconds))
            (/ (/ iters (- end-time start-time)) 1000)))]))
