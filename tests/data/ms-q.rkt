42f325a985499d4a4213d63226baf46a2181b9ff
Aaron Turon <turon@ccs.neu.edu>
2012-07-16 14:52:47 -0400

    testing for ms-q

---
+++ tests/data/ms-q.rkt
@@ -0,0 +1,28 @@
+#lang racket/base
+
+(require "../../data/ms-q.rkt" 
+	 "../test-tools.rkt"
+	 rackunit)
+
+(define q (make-queue))
+
+(check-equal? (deq! q #f) #f "deq! on empty queue not failure-result")
+
+(enq! q 1)
+(check-equal? (deq! q) 1 "deq! on singleton queue not singleton")
+(check-equal? (deq! q #f) #f "deq! on empty queue not failure-result")
+
+(enq! q 1)
+(check-equal? (deq! q #f) 1 "deq! on singleton queue not singleton")
+(check-equal? (deq! q #f) #f "deq! on empty queue not failure-result")
+
+(enq! q 1)
+(enq! q 2)
+(check-equal? (deq! q) 1 "deq! on two-element queue not top element")
+(check-equal? (deq! q) 2 "second deq! on two-element queue not bottom element")
+(check-equal? (deq! q #f) #f "deq! on empty queue not failure-result")
+
+(multithreaded-for-effect 2
+  (begin (enq! q 1)
+	 (deq! q)))
+(check-equal? (deq! q #f) #f "simple multithreaded enq!/deq! test failed")
--- tests/test-all.rkt
+++ tests/test-all.rkt
@@ -1,4 +1,5 @@
 #lang racket/base
 
 (require "data/treiber.rkt"
+	 "data/ms-q.rkt"
 	 "by-hand/treiber-stack.rkt")
